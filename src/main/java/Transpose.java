61075212ae56e04a40be2658704953404a4c2186
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2020-03-17 00:48:15 +0300

    README.md + little improvements

--- src/main/java/Transpose.java
+++ src/main/java/Transpose.java
@@ -3,36 +3,54 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Transpose {
+
+    private File outputFile;
+    private File inputFile;
+
     @Option(name = "-a", usage = "Width of a field for a word.")
     private int num = 10;
 
     @Option(name = "-t", usage = "Cuts the word if it doesn't fit in the size set by '-a'.")
-    private boolean cutWords;
+    private boolean cutWords = false;
 
     @Option(name = "-r", usage = "Aligns the word to the right.")
-    private boolean alignRight;
+    private boolean alignRight = false;
 
     @Option(name = "-o", usage = "Sets an output file, else STDOUT.")
-    private String outputFile;
+    private void setOutputFile(File file) throws IOException {
+        if (file.exists() && file.canWrite() || file.createNewFile()) {
+            outputFile = file;
+        } else {
+            throw new FileNotFoundException();
+        }
+    }
 
     @Argument(usage = "Sets an input file, else STDIN.")
-    private String inputFileName;
+    private void setInputFile(File file) throws FileNotFoundException {
+        if (file.exists() && file.isFile() && file.canRead()) {
+            inputFile = file;
+        } else {
+            throw new FileNotFoundException();
+        }
+    }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         new Transpose().launch(args);
     }
 
-    private void launch(String[] args) throws IOException {
+    private void launch(String[] args) {
         CmdLineParser parser = new CmdLineParser(this);
 
         try {
             parser.parseArgument(args);
         } catch (CmdLineException e) {
             System.err.println(e.getMessage());
-            System.err.println("java -jar transpose.jar [-a width] [-t] [-r] [-o outputFile] [inputFile]");
+            System.err.println("transpose [-a width] [-t] [-r] [-o outputFile] [inputFile]");
             parser.printUsage(System.err);
         }
     }
