26a66c50b7e021a4be91843894e111d1fbe2f0ab
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2020-03-17 13:05:29 +0300

    Now it can read words

--- src/main/java/Transpose.java
+++ src/main/java/Transpose.java
@@ -3,9 +3,9 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Transpose {
 
@@ -13,7 +13,7 @@ public class Transpose {
     private File inputFile;
 
     @Option(name = "-a", usage = "Width of a field for a word.")
-    private int num = 10;
+    private int num;
 
     @Option(name = "-t", usage = "Cuts the word if it doesn't fit in the size set by '-a'.")
     private boolean cutWords = false;
@@ -39,11 +39,11 @@ public class Transpose {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Transpose().launch(args);
     }
 
-    private void launch(String[] args) {
+    private void launch(String[] args) throws IOException {
         CmdLineParser parser = new CmdLineParser(this);
 
         try {
@@ -52,6 +52,29 @@ public class Transpose {
             System.err.println(e.getMessage());
             System.err.println("transpose [-a width] [-t] [-r] [-o outputFile] [inputFile]");
             parser.printUsage(System.err);
+            return;
         }
+
+        BufferedReader input = (inputFile == null) ? new BufferedReader(new InputStreamReader(System.in)) : new BufferedReader(new FileReader(inputFile));
+        BufferedWriter output = (outputFile == null) ? new BufferedWriter(new OutputStreamWriter(System.out)) : new BufferedWriter(new FileWriter(outputFile));
+        for (ArrayList<String> l : readWords(input)) {
+            System.out.println(l);
+        }
+
     }
+
+    private ArrayList<ArrayList<String>> readWords(BufferedReader in) throws IOException {
+        ArrayList<ArrayList<String>> words = new ArrayList<>();
+        try (in) {
+            String line = in.readLine();
+            while (line != null) {
+                if (!line.isBlank()) {
+                    words.add(new ArrayList<>(Arrays.asList(line.trim().split(" +"))));
+                }
+                line = in.readLine();
+            }
+        }
+        return words;
+    }
+
 }
