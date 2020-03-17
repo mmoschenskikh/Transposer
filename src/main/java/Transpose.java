07ae13799d66bb4f752343865287901fa214dfb6
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2020-03-17 20:03:03 +0300

    Now it can both read & write words

--- src/main/java/Transpose.java
+++ src/main/java/Transpose.java
@@ -15,10 +15,10 @@ public class Transpose {
     @Option(name = "-a", usage = "Width of a field for a word.")
     private int num;
 
-    @Option(name = "-t", usage = "Cuts the word if it doesn't fit in the size set by '-a'.")
+    @Option(name = "-t", usage = "Cuts words if they don't fit in the size set by '-a'.")
     private boolean cutWords = false;
 
-    @Option(name = "-r", usage = "Aligns the word to the right.")
+    @Option(name = "-r", usage = "Aligns words to the right.")
     private boolean alignRight = false;
 
     @Option(name = "-o", usage = "Sets an output file, else STDOUT.")
@@ -57,24 +57,55 @@ public class Transpose {
 
         BufferedReader input = (inputFile == null) ? new BufferedReader(new InputStreamReader(System.in)) : new BufferedReader(new FileReader(inputFile));
         BufferedWriter output = (outputFile == null) ? new BufferedWriter(new OutputStreamWriter(System.out)) : new BufferedWriter(new FileWriter(outputFile));
-        for (ArrayList<String> l : readWords(input)) {
-            System.out.println(l);
-        }
 
     }
 
     private ArrayList<ArrayList<String>> readWords(BufferedReader in) throws IOException {
         ArrayList<ArrayList<String>> words = new ArrayList<>();
+        int maxLength = 0;
+
         try (in) {
             String line = in.readLine();
             while (line != null) {
                 if (!line.isBlank()) {
-                    words.add(new ArrayList<>(Arrays.asList(line.trim().split(" +"))));
+                    ArrayList<String> row = new ArrayList<>(Arrays.asList(line.trim().split(" +")));
+                    words.add(row);
+                    maxLength = Integer.max(maxLength, row.size());
                 }
                 line = in.readLine();
+            }
+        }
+
+        ArrayList<String> end = new ArrayList<>();
+        for (int i = 0; i < maxLength; i++) {
+            end.add("");
+        }
+
+        for (ArrayList<String> row : words) {
+            int size = row.size();
+            if (size < maxLength) {
+                row.addAll(end.subList(0, maxLength - size));
             }
         }
         return words;
     }
 
+    private void writeWords(ArrayList<ArrayList<String>> words, BufferedWriter out) throws IOException {
+        final int rows = words.size();
+        final int columns = words.get(0).size();
+        try (out) {
+            for (int i = 0; i < columns; i++) {
+                StringBuilder line = new StringBuilder();
+                for (int j = 0; j < rows; j++) {
+                    String word = words.get(j).get(i);
+                    if (!word.isEmpty()) {
+                        line.append(word);
+                        line.append((j == rows - 1) ? "" : " ");
+                    }
+                }
+                out.write(line.toString());
+                out.newLine();
+            }
+        }
+    }
 }
